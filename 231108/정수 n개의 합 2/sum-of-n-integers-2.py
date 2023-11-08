08c38ec13e4c421f0496c33a164fd56ad7eaf5ca
minji <62049151+meanz1@users.noreply.github.com>
2023-11-09 00:34:14 +0900

    Title: 정수 n개의 합 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231108/정수 n개의 합 2/sum-of-n-integers-2.py
+++ 231108/정수 n개의 합 2/sum-of-n-integers-2.py
@@ -7,7 +7,7 @@ prefix_sum = [0]*n
 prefix_sum[0] = nums[0]
 for i in range(1, n):
     prefix_sum[i] = prefix_sum[i-1]+nums[i]
-result = -100
-for i in range(n-k+2):
+result = -sys.maxsize
+for i in range(n-k):
     result = max(result, prefix_sum[i+5]-prefix_sum[i])
 print(result)
