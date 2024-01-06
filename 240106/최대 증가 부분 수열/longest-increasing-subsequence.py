334881ce836130153b71b39644dc3bba9a0a157e
minji <62049151+meanz1@users.noreply.github.com>
2024-01-06 19:32:46 +0900

    Title: 최대 증가 부분 수열, Time: 131ms, Memory: 25MB, Status: Passed - CodeTree

--- 240106/최대 증가 부분 수열/longest-increasing-subsequence.py
+++ 240106/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -1,9 +1,8 @@
 n = int(input())
-seq = list(map(int, input().split()))
-dp = [0]*(n)
-dp[0] = 1
+seq = [0]+ list(map(int, input().split()))
+dp = [0]*(n+1)
 
-for i in range(n):
+for i in range(1, n+1):
     for j in range(i):
         if seq[j] < seq[i]:
             dp[i] = max(dp[i], dp[j]+1)
