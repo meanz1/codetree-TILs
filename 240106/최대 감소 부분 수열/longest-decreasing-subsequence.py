9d37bb675d671dc44c9be10f4afa7fc792681c96
minji <62049151+meanz1@users.noreply.github.com>
2024-01-06 19:43:34 +0900

    Title: 최대 감소 부분 수열, Time: 145ms, Memory: 28MB, Status: Passed - CodeTree

--- 240106/최대 감소 부분 수열/longest-decreasing-subsequence.py
+++ 240106/최대 감소 부분 수열/longest-decreasing-subsequence.py
@@ -1,10 +1,10 @@
 n = int(input())
 seq = [0]+list(map(int, input().split()))
-dp = [0]*(n+1)
-dp[1] = 1
+dp = [10001]*(n+1)
+
 for i in range(1, n+1):
     for j in range(i):
         if seq[j] > seq[i]:
             dp[i] = max(dp[i], dp[j]+1)
 
-print(max(dp))
+print(max(dp)-10000)
