de9637fe780be45b28d78e39fd69b4217a7e08f4
minji <62049151+meanz1@users.noreply.github.com>
2024-01-06 20:15:23 +0900

    Title: 최대 점프 횟수, Time: 116ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240106/최대 점프 횟수/maximum-number-of-jumps.py
+++ 240106/최대 점프 횟수/maximum-number-of-jumps.py
@@ -2,11 +2,19 @@ n = int(input())
 jumps = list(map(int, input().split()))
 
 dp = [0]*n
-
+flag = n
 for i in range(n):
     for j in range(i+1, i+jumps[i]+1):
         if j >= n:
             break
         dp[j] = max(dp[i]+1, dp[j])
 
-print(max(dp))
+new_dp = dp[1:]
+for i in range(n-1):
+    if new_dp[i] == 0:
+        flag = i
+        break
+if flag != 5:
+    print(max(new_dp[:i]))
+else:
+    print(max(new_dp))
